dcdcc6606bcb519a3999a008c59f5befe5af8031
Tomasz Zieba <tomasz.zieba89@gmail.com>
2021-05-14 15:04:37 +0200

    added HeaderCartButton component and svg CartIcon component

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,5 +1,7 @@
 import mealsImage from '../../assets/meals.jpg';
 
+import HeaderCartButton from './HeaderCartButton';
+
 import classes from './Header.module.scss';
 
 const Header: React.FC = () => {
@@ -7,7 +9,7 @@ const Header: React.FC = () => {
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of delicious food!" />
